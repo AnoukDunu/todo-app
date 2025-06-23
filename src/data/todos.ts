7d48c927fd3e9cbcfd3d2f9991503242e66f8a84
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2025-06-23 16:44:29 +1000

    inserted 3 dummy todos to render

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+// inserting data here for the todo app
+
+export const todos = [
+  {
+    id: 1,
+    title: 'Learn React',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Build a Todo App',
+    completed: false,
+  },
+];
