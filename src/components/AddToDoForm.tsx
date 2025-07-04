e40e733dd98a98848c961a6887948aeb81b32caa
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2025-07-04 12:04:49 +1000

    creating a controlled input field

--- src/components/AddToDoForm.tsx
+++ src/components/AddToDoForm.tsx
@@ -1,10 +1,19 @@
 // this component is to add Todos into the list.
 // Created as a separate component as it has its own logic and state management
 
+import { useState } from "react"
+
 export default function AddToDoForm() {
+
+    //turning our input field into a controlled component
+    //new state variable to hold the value of the input field
+    const [inputValue, setInputValue] = useState ("");
     return (
         <form className="flex">
-            <input 
+            <input
+                // this changes the value of the input field into a controlled input
+                //by 'this' I mean the VALUE attributed added to the input field
+                value={inputValue}
                 placeholder="Add a new todo"
                 className="rounded-s-md grow border border-gray-400 p-2"
             />
