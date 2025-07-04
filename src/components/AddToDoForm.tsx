b71a5e4d4f26bdb973fc9bdb5325c4c83eb5c78b
AnoukDunu <52993706+AnoukDunu@users.noreply.github.com>
2025-07-04 11:51:03 +1000

    customised the button and input field

--- src/components/AddToDoForm.tsx
+++ src/components/AddToDoForm.tsx
@@ -6,8 +6,11 @@ export default function AddToDoForm() {
         <form>
             <input 
                 placeholder="Add a new todo"
-                className="rounded-s-md"
+                className="rounded-s-md grow border border-gray-400 p-2"
             />
+            <button type="submit" className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800">
+                Add Todo
+            </button>
         </form>
     )
 }
